009683c0a8218854d28d618fbac7e76aa32d3f64
PERROUAS Thibault <thibaultperrouas8@gmail.com>
2024-05-04 17:58:15 +0200

    Revert "modif index"
    This reverts commit ffbcde257b099e67814554b9eb1f3d4ca239c9c3.

--- index.php
+++ index.php
@@ -51,20 +51,13 @@ function showMenu()
                 writeBooksToJson($bookCollection->toArray(), $filename);
                 break;
             case 2:
-                    echo "Vous avez choisi 'Modifier un livre'\n";
-                    echo "Entrez l'identifiant du livre à modifier: ";
-                    $bookID = intval(readline());
-                    $bookToModif = $bookCollection->findByKey($bookID);
-                
-                    if ($bookToModif !== null) {
-                        $bookCollection->showSingleBook($bookToModif);
-                        $bookCollection->modifBook($bookToModif);
-                        writeBooksToJson($bookCollection->toArray(), $filename);
-                        echo "Les modifications ont été enregistrées avec succès.\n";
-                    } else {
-                        echo "Aucun livre trouvé avec cet identifiant.\n";
-                    }
-                    break;
+                echo "Vous avez choisi 'Modifier un livre'\n";
+                echo "Entrez l'identifiant du livre à modifier: ";
+                $bookID = intval(readline());
+                $bookToModif = $bookCollection->findByKey($bookID);
+                $bookCollection->showSingleBook($bookToModif);
+                $bookCollection->modifBook($bookToModif);
+                break;
             case 3:
 
                 echo "Vous avez choisi 'Supprimer un livre'\n";
