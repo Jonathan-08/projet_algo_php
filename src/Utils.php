71112dff6de6255eee60f3a311758c2fbd673647
Jonathan Assi <assijonathan2@gmail.com>
2024-05-14 23:09:14 +0200

    Amélioration des fonctions dans Utils.php

--- src/Utils.php
+++ src/Utils.php
@@ -4,18 +4,31 @@ namespace App\Algo;
 use App\Algo\Heap\Heap;
 use App\Algo\Book;
 
+/**
+ * Cette classe regroupe toutes les fonctions utilse au bon fonctionnement de la bookCollection
+ */
+
 class Utils{
     static function fetchBooksFromFile($filename){
-        $json_data = file_get_contents($filename);
-        $data = json_decode($json_data, true);
-        return $data;
+        if(file_exists($filename)){
+            $json_data = file_get_contents($filename);
+            $data = json_decode($json_data, true);
+            return $data;
+        } else {
+            die("Le fichier $filename n'existe pas");
+        }
+        
     }
 
     static function fill(Heap $bookCollection, $filename): void{
         $fetchedBooks = self::fetchBooksFromFile($filename);
-        foreach($fetchedBooks as $fetchedBook){
-        $book = new Book($fetchedBook["name"], $fetchedBook["description"], $fetchedBook["available"], $fetchedBook["id"]);
-        $bookCollection->push($book);
+        if(!empty($fetchedBooks) && $fetchedBooks !== NULL){
+            foreach($fetchedBooks as $fetchedBook){
+                $book = new Book($fetchedBook["name"], $fetchedBook["description"], $fetchedBook["available"], $fetchedBook["id"]);
+                $bookCollection->push($book);
+            }
+        } else {
+            die("Le fichier: $filename est vide.");
         }
     }
 }
