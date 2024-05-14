c8b90d5dbb2b44719f5cdf3228fd447b383844f0
Jonathan Assi <assijonathan2@gmail.com>
2024-05-15 00:37:00 +0200

    - Le fichier BookCollection est dorénavant une classe qui regroupe toutes les fonctions utiles à la bookCollection
    - les fonction remove et findByKey sont à revoir
    - la fonction findById permet de retrouver un
    livre en fonction de son Id. Elle retourne le
    type LinkedListvalue car on en aura besoin pour
    les suppressions.

--- src/Heap/Heap.php
+++ src/Heap/Heap.php
@@ -8,18 +8,6 @@ use App\Algo\LinkedListValue;
 
 class Heap extends LinkedList{
 
-    public function toArray(): array {
-        $booksArray = [];
-        $current = $this->first;
-    
-        while ($current !== null) {
-            $booksArray[] = $current->value;
-            $current = $current->next;
-        }
-    
-        return $booksArray;
-    }
-
     public function pop(): mixed
     {
         if ($this->first === null) {
@@ -39,81 +27,5 @@ class Heap extends LinkedList{
 
         return $first->value;
     }
-
-    public function getLength(): int {
-        $count = 0;
-        $current = $this->first; 
-    
-        while ($current !== null) {
-            $count++;
-            $current = $current->next;
-        }
-    
-        return $count;
-    }
-
-
-    public function remove(Book $bookToRemove): void {
-        $previous = null;
-        $current = $this->first;
-    
-        while ($current !== null) {
-            if ($current->value === $bookToRemove) {
-                if ($previous === null) {
-                    $this->first = $current->next;
-                } else {
-                    $previous->next = $current->next;
-                }
-                return;
-            }
-            $previous = $current;
-            $current = $current->next;
-        }
-    }
-    
-    public function findByKey(string $searchKey): ?Book {
-        $current = $this->first;
-    
-        while ($current !== null) {
-            if ($current->value->name === $searchKey ||
-                $current->value->description === $searchKey ||
-                $current->value->id == $searchKey ||
-                $current->value->available == $searchKey) {
-                return $current->value;
-            }
-            $current = $current->next;
-        }
-    
-        return null;
-    }
-
-    public function showSingleBook($bookValue): void{
-        $bookAvailable = $bookValue->available ? "Disponible" : "Indisponible";
-        echo "Nom du livre: $bookValue->name\nDescription: $bookValue->description\nDisponibilité: $bookAvailable\n";
-    }
-
-    public function showAllBooks(): void{
-        $current = $this->first;
-
-        if($this->first === null){
-            echo "La bibliothèque est vide\n";
-        }
-
-        while($current !== null){
-            $bookAvailable = $current->value->available ? "Disponible" : "Indisponible";
-            echo "Nom du livre: {$current->value->name}\nDescription: {$current->value->description}\nDisponibilité: {$bookAvailable}\n\n";
-            $current = $current->next;
-        }
-    }
-
-    public function modifBook($bookValue): void{
-        echo "Nouveau nom: ";
-        $bookValue->name = readline();
-        echo "Nouvelle description nom: ";
-        $bookValue->description = readline();
-        echo "Nouvelle disponibilité: ";
-        $bookValue->available = intval(readline());
-    }
-
     
 }
